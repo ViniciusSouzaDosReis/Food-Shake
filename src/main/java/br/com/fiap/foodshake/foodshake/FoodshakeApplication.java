26d696f0dd8b17c31033e05d70c9384c2a5593de
Vinicius <vinireis010305@gmail.com>
2023-03-09 18:44:41 -0300

    Criando API em Spring Boot

---
+++ src/main/java/br/com/fiap/foodshake/foodshake/FoodshakeApplication.java
@@ -0,0 +1,13 @@
+package br.com.fiap.foodshake.foodshake;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodshakeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodshakeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/fiap/foodshake/foodshake/FoodshakeApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fiap.foodshake.foodshake;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodshakeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
