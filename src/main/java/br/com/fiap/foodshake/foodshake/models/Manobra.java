11b767e6e8c2386a008b6d8df00fea2250f18cfd
Vinicius <vinireis010305@gmail.com>
2023-03-09 18:50:15 -0300

    Criando Manobra Model

---
+++ src/main/java/br/com/fiap/foodshake/foodshake/models/Manobra.java
@@ -0,0 +1,28 @@
+package br.com.fiap.foodshake.foodshake.models;
+
+public class Manobra {
+  private String nome;
+  private int quantidade;
+
+  public Manobra(String nome, int quantidade) {
+    this.nome = nome;
+    this.quantidade = quantidade;
+  }
+
+  public String getNome() {
+    return nome;
+  }
+
+  public void setNome(String nome) {
+    this.nome = nome;
+  }
+
+  public int getQuantidade() {
+    return quantidade;
+  }
+
+  public void setQuantidade(int quantidade) {
+    this.quantidade = quantidade;
+  }
+
+}
