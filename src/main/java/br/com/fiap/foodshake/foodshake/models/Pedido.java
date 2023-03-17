f7fc9dfa0c4d141931f04f4ca585c7e7322add46
Vinicius <vinireis010305@gmail.com>
2023-03-17 10:02:58 -0300

    Alterando Contrutor Pedido

--- src/main/java/br/com/fiap/foodshake/foodshake/models/Pedido.java
+++ src/main/java/br/com/fiap/foodshake/foodshake/models/Pedido.java
@@ -6,14 +6,17 @@ public class Pedido {
   private long id;
   private long idUsuario;
   private Local localEntrega;
-  private int  alimentos[];
+  private int alimentos[];
   private BigDecimal valor;
   private Manobra manobras[];
   private boolean entregue;
   private boolean finalizado;
-  
-  public Pedido(long id, long idUsuario, Local localEntrega, int[] alimentos, BigDecimal valor,
-      Manobra[] manobras) {
+
+  private Pedido() {
+
+  }
+
+  public Pedido(long id, long idUsuario, Local localEntrega, int[] alimentos, BigDecimal valor, Manobra[] manobras) {
     this.id = id;
     this.idUsuario = idUsuario;
     this.localEntrega = localEntrega;
@@ -21,56 +24,81 @@ public class Pedido {
     this.valor = valor;
     this.manobras = manobras;
   }
-  
-  public long getId() {
-    return id;
-  }
-  public void setId(long id) {
+
+  public Pedido(long id, long idUsuario, Local localEntrega, int[] alimentos, BigDecimal valor, Manobra[] manobras,
+      boolean entregue, boolean finalizado) {
     this.id = id;
-  }
-  public long getIdUsuario() {
-    return idUsuario;
-  }
-  public void setIdUsuario(long idUsuario) {
     this.idUsuario = idUsuario;
-  }
-  public Local getLocalEntrega() {
-    return localEntrega;
-  }
-  public void setLocalEntrega(Local localEntrega) {
     this.localEntrega = localEntrega;
-  }
-  public int[] getAlimentos() {
-    return alimentos;
-  }
-  public void setAlimentos(int[] alimentos) {
     this.alimentos = alimentos;
-  }
-  public BigDecimal getValor() {
-    return valor;
-  }
-  public void setValor(BigDecimal valor) {
     this.valor = valor;
-  }
-  public Manobra[] getManobras() {
-    return manobras;
-  }
-  public void setManobras(Manobra[] manobras) {
     this.manobras = manobras;
-  }
-  public boolean isEntregue() {
-    return entregue;
-  }
-  public void setEntregue(boolean entregue) {
     this.entregue = entregue;
-  }
-  public boolean isFinalizado() {
-    return finalizado;
-  }
-  public void setFinalizado(boolean finalizado) {
     this.finalizado = finalizado;
   }
 
-  
+  public long getId() {
+    return id;
+  }
+
+  public void setId(long id) {
+    this.id = id;
+  }
+
+  public long getIdUsuario() {
+    return idUsuario;
+  }
+
+  public void setIdUsuario(long idUsuario) {
+    this.idUsuario = idUsuario;
+  }
+
+  public Local getLocalEntrega() {
+    return localEntrega;
+  }
+
+  public void setLocalEntrega(Local localEntrega) {
+    this.localEntrega = localEntrega;
+  }
+
+  public int[] getAlimentos() {
+    return alimentos;
+  }
+
+  public void setAlimentos(int[] alimentos) {
+    this.alimentos = alimentos;
+  }
+
+  public BigDecimal getValor() {
+    return valor;
+  }
+
+  public void setValor(BigDecimal valor) {
+    this.valor = valor;
+  }
+
+  public Manobra[] getManobras() {
+    return manobras;
+  }
+
+  public void setManobras(Manobra[] manobras) {
+    this.manobras = manobras;
+  }
+
+  public boolean isEntregue() {
+    return entregue;
+  }
+
+  public void setEntregue(boolean entregue) {
+    this.entregue = entregue;
+  }
+
+  public boolean isFinalizado() {
+    return finalizado;
+  }
+
+  public void setFinalizado(boolean finalizado) {
+    this.finalizado = finalizado;
+  }
 
 }
