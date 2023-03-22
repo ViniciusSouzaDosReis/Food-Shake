ccb7fb635beba0acdbb476b17671ef7275958fde
Vinicius <vinireis010305@gmail.com>
2023-03-22 18:46:41 -0300

    Criando ManobraRepository

---
+++ src/main/java/br/com/fiap/foodshake/foodshake/repository/ManobraRepository.java
@@ -0,0 +1,8 @@
+package br.com.fiap.foodshake.foodshake.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import br.com.fiap.foodshake.foodshake.models.Manobra;
+
+public interface ManobraRepository extends JpaRepository<Manobra, Long>{
+  
+}
