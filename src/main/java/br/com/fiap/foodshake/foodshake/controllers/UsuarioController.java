7081732a9888f9fe036b433038a9015a42fd67ef
Vinicius <vinireis010305@gmail.com>
2023-03-24 08:33:55 -0300

    Alterando Controller de Usuario

--- src/main/java/br/com/fiap/foodshake/foodshake/controllers/UsuarioController.java
+++ src/main/java/br/com/fiap/foodshake/foodshake/controllers/UsuarioController.java
@@ -2,10 +2,13 @@ package br.com.fiap.foodshake.foodshake.controllers;
 
 import java.time.LocalDate;
 import java.util.ArrayList;
+import java.util.List;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,95 +16,68 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.fiap.foodshake.foodshake.models.Local;
 import br.com.fiap.foodshake.foodshake.models.Usuario;
+import br.com.fiap.foodshake.foodshake.repository.UsuarioRepository;
 
 @RestController
+@RequestMapping("/api/usuario")
 public class UsuarioController {
-  ArrayList<Usuario> usuarios = new ArrayList<Usuario>();
+
   Logger log = LoggerFactory.getLogger(UsuarioController.class);
 
-  @GetMapping("/api/usuario")
-  public ArrayList<Usuario> show() {
-    if (usuarios.isEmpty()) {
-      Local local = new Local(
-          "73060-530",
-          "Quadra AR 5 Área Especial 04",
-          "Setor Oeste (Sobradinho)",
-          "20",
-          "8 andar",
-          "Brasília",
-          "Distrito Federal");
+  @Autowired
+  UsuarioRepository repository;
 
-      usuarios.add(new Usuario(
-          1,
-          "Felipes",
-          "DaSilva",
-          LocalDate.now(),
-          "714.713.648-95",
-          "cleiton",
-          "(11)95043-3454",
-          "senhadificil123",
-          local));
-    }
-
-    return usuarios;
+  @GetMapping
+  public List<Usuario> index() {
+    return repository.findAll();
   }
 
-  @GetMapping("/api/usuario/{id}")
+  @GetMapping("{id}")
   public ResponseEntity<Usuario> show(@PathVariable long id){
-    var usuarioExistente = usuarios
-      .stream()
-      .filter(u -> u.getId() == id)
-      .findFirst();
-    
-    if(usuarioExistente.isEmpty()){
-      return ResponseEntity.notFound().build();
-    }
-    return ResponseEntity.ok(usuarioExistente.get());
+    log.info("detalhando usuario: " + id);
+    var usuarioEncontrado = repository.findById(id);
+
+    if (usuarioEncontrado.isEmpty()) return ResponseEntity.notFound().build();
+
+    return ResponseEntity.ok(usuarioEncontrado.get());
   }
 
-  @PostMapping("/api/usuario")
+  @PostMapping
   public ResponseEntity<Usuario> create(@RequestBody Usuario usuario) {
-    usuario.setId(usuarios.size() + 1l);
-    log.info("cadastrando alimento: " + usuario);
-    usuarios.add(usuario);
+    log.info("cadastrando usuairo: " + usuario);
+    repository.save(usuario);
 
     return ResponseEntity.status(HttpStatus.CREATED).body(usuario);
   }
   
-  @DeleteMapping("/api/usuario/{id}")
-  public ResponseEntity<Usuario> delete(@PathVariable long id){
-    var usuarioExistente = usuarios
-      .stream()
-      .filter(u -> u.getId() == id)
-      .findFirst();
+  @DeleteMapping("{id}")
+  public ResponseEntity<Usuario> destroy(@PathVariable long id){
+    log.info("apagando usuario: " + id);
+    var usuarioEncontrado = repository.findById(id);
 
-   if(usuarioExistente.isEmpty()){
-      return ResponseEntity.notFound().build();
-    }
+    if (usuarioEncontrado.isEmpty()) return ResponseEntity.notFound().build();
 
-    usuarios.remove(usuarioExistente.get());
+    repository.delete(usuarioEncontrado.get());
+
     return ResponseEntity.noContent().build();
   }
 
-  @PutMapping("/api/usuario/{id}")
+  @PutMapping("{id}")
   public ResponseEntity<Usuario> update(@PathVariable long id, @RequestBody Usuario usuario){
-    var usuarioExistente = usuarios
-      .stream()
-      .filter(u -> u.getId() == id)
-      .findFirst();
-    
-    if(usuarioExistente.isEmpty()){
-      return ResponseEntity.notFound().build();
-    }
+    log.info("autalizando usuario: " + id);
+    var usuarioEncontrado = repository.findById(id);
+
+    if (usuarioEncontrado.isEmpty()) return ResponseEntity.notFound().build();
 
     usuario.setId(id);
-    usuarios.remove(usuarioExistente.get());
-    usuarios.add(usuario);
+    repository.save(usuario);
 
     return ResponseEntity.ok(usuario);
+
   }
 }
