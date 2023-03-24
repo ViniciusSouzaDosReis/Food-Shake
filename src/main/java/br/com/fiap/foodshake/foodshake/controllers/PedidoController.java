72219524f3094c0ac8c6c46c9691186251887bec
Vinicius <vinireis010305@gmail.com>
2023-03-24 08:33:48 -0300

    Alterando Controller de Pedido

--- src/main/java/br/com/fiap/foodshake/foodshake/controllers/PedidoController.java
+++ src/main/java/br/com/fiap/foodshake/foodshake/controllers/PedidoController.java
@@ -2,9 +2,14 @@ package br.com.fiap.foodshake.foodshake.controllers;
 
 import java.math.BigDecimal;
 import java.util.ArrayList;
+import java.util.List;
 
+import javax.net.ssl.HttpsURLConnection;
+
+import org.apache.catalina.connector.Response;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -13,98 +18,70 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.fiap.foodshake.foodshake.models.Local;
 import br.com.fiap.foodshake.foodshake.models.Manobra;
 import br.com.fiap.foodshake.foodshake.models.Pedido;
+import br.com.fiap.foodshake.foodshake.repository.PedidoRepository;
 
 @RestController
+@RequestMapping("/api/pedido")
 public class PedidoController {
-  ArrayList<Pedido> pedidos = new ArrayList<Pedido>();
+
   Logger log = LoggerFactory.getLogger(PedidoController.class);
 
-  @GetMapping("/api/pedido")
-  public ArrayList<Pedido> show() {
-    if (pedidos.isEmpty()) {
-      Local local = new Local(
-          "73060-530",
-          "Quadra AR 5 Área Especial 04",
-          "Setor Oeste (Sobradinho)",
-          "20",
-          "8 andar",
-          "Brasília",
-          "Distrito Federal");
+  @Autowired
+  PedidoRepository repository;
 
-      Manobra m1 = new Manobra("Flip Legal", 1);
-      Manobra m2 = new Manobra("Flip Muito Legal", 2);
-      Manobra m3 = new Manobra("Flip Meio Legal", 4);
-      Manobra[] manobras = { m1, m2, m3 };
-
-      int[] alimentosId = { 1, 3, 10 };
-
-      pedidos.add(new Pedido(
-          1,
-          1,
-          local,
-          alimentosId,
-          new BigDecimal(12.32),
-          manobras));
-    }
-
-    return pedidos;
+  @GetMapping
+  public List<Pedido> index() {
+    return repository.findAll();
   }
 
-  @GetMapping("/api/pedido/{id}")
+  @GetMapping("{id}")
   public ResponseEntity<Pedido> show(@PathVariable long id) {
-    var pedidoExistente = pedidos
-        .stream()
-        .filter(a -> a.getId() == id)
-        .findFirst();
+    log.info("detalhando pedido: " + id);
+    var pedidoEncontrado = repository.findById(id);
 
-    if (pedidoExistente.isEmpty()) {
+    if (pedidoEncontrado.isEmpty())
       return ResponseEntity.notFound().build();
-    }
-    return ResponseEntity.ok(pedidoExistente.get());
+    
+    return ResponseEntity.ok(pedidoEncontrado.get());
   }
 
-  @PostMapping("/api/pedido")
+  @PostMapping
   public ResponseEntity<Pedido> create(@RequestBody Pedido pedido) {
-    pedido.setId(pedidos.size() + 1l);
-    log.info("cadastrando pedido: " + pedido);
-    pedidos.add(pedido);
+    log.info("Cadastrando pedido: " + pedido);
+    repository.save(pedido);
 
     return ResponseEntity.status(HttpStatus.CREATED).body(pedido);
   }
 
-  @DeleteMapping("/api/pedido/{id}")
-  public ResponseEntity<Pedido> delete(@PathVariable long id) {
-    var pedidoExistente = pedidos
-        .stream()
-        .filter(a -> a.getId() == id)
-        .findFirst();
-    if (pedidoExistente.isEmpty()) {
-      return ResponseEntity.notFound().build();
-    }
+  @DeleteMapping("{id}")
+  public ResponseEntity<Pedido> destroy(@PathVariable long id) {
+    log.info("apagando pedido: " + id);
+    var pedidoEncontrado = repository.findById(id);
 
-    pedidos.remove(pedidoExistente.get());
+    if (pedidoEncontrado.isEmpty())
+      return ResponseEntity.notFound().build();
+    
+    repository.delete(pedidoEncontrado.get());
+
     return ResponseEntity.noContent().build();
   }
 
-  @PutMapping("/api/pedido/{id}")
+  @PutMapping("{id}")
   public ResponseEntity<Pedido> update(@PathVariable long id, @RequestBody Pedido pedido) {
-    var pedidoExistente = pedidos
-        .stream()
-        .filter(a -> a.getId() == id)
-        .findFirst();
+    log.info("atualizando pedido: " + id);
+    var pedidoEncontrado = repository.findById(id);
 
-    if (pedidoExistente.isEmpty()) {
+    if (pedidoEncontrado.isEmpty())
       return ResponseEntity.notFound().build();
-    }
-
+    
     pedido.setId(id);
-    pedidos.remove(pedidoExistente.get());
-    pedidos.add(pedido);
+    repository.save(pedido);
 
     return ResponseEntity.ok(pedido);
   }
